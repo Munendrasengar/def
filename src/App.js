abb35a75f508c3f76718c631b8019c5d54dc7dfa
Munendrasengar <aaryansengar5@gmail.com>
2022-01-02 16:26:55 +0530

    .

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Asite from "./Asite";
+
+function App() {
+  return (
+    <>
+    <Asite/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Asite.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Asite = () => {
+    return (
+        <div>
+            <h1>Welcome to my world</h1>
+        </div>
+    )
+}
+
+export default Asite
